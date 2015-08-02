f299efec155a809242f4c8574e9fab13d425b82f
gaotongfei <gaotongfei7@163.com>
2015-08-02 12:23:51 +0800

    problem solved after install requests[security]

--- helper.py
+++ helper.py
@@ -9,26 +9,26 @@ url_prefix = 'https://api.telegram.org/bot' + tg_bot_token + '/'
 
 def getMe():
     url = url_prefix + 'getMe'
-    r = requests.get(url, verify=False)
+    r = requests.get(url)
     print(r.status_code)
     print(r.text)
 
 def sendMessage(chat_id, text):
     url = url_prefix + 'sendMessage?chat_id={0}&text={1}'.format(chat_id, text)
-    r = requests.get(url, verify=False)
+    r = requests.get(url)
     return r.json()
 
 def getUpdates():
     url = url_prefix + 'getUpdates'
-    r = requests.get(url, verify=False)
+    r = requests.get(url)
     return r.json()
 
 def google_api(message):
     url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + message
-    r = requests.get(url, verify=False)
+    r = requests.get(url)
     return r.json()
 
 def youdao_api(message):
     url = 'http://fanyi.youdao.com/openapi.do?keyfrom={}&key={}&type=data&doctype=json&version=1.1&q={}'.format(youdao_keyfrom, youdao_key, message)
-    r = requests.get(url, verify=False)
+    r = requests.get(url)
     return r.json()
