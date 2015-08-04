a532bee24aef428b743517c5caa0a2d599a4ea7f
gaotongfei <gaotongfei7@163.com>
2015-08-04 21:14:04 +0800

    add comment

--- helper.py
+++ helper.py
@@ -1,15 +1,38 @@
 import requests
 import os
 
+'''
+:param tg_bot_toKen: telegram bot 的 api token，在此处
+  https://core.telegram.org/bots/api#authorizing-your-bot
+  阅读如何获得token
+
+:param youdao_keyfrom: 有道词典api key，可在此处申请
+  http://fanyi.youdao.com/openapi?path=data-mode
+
+:param youdao_key: 同上
+
+:param nasa_api_key: https://api.nasa.gov/index.html#apply-for-an-api-key 此处申请
+
+'''
+
+# 你可以通过在命令行中输入export tg_bot_token=your-telegram-bot-token来配置你的token，
+# 同理youdao_keyfrom，youdao_key，nasa_api_key也可以这么配置，如果你不喜欢这种方式，可以直接改为明文。
+
 tg_bot_token = os.environ.get('tg_bot_token')
 youdao_keyfrom = os.environ.get('youdao_keyfrom')
 youdao_key = os.environ.get('youdao_key')
 nasa_api_key = os.environ.get('nasa_api_key')
 
+'''
+:param url_prefix: https://core.telegram.org/bots/api#making-requests 阅读此处
+'''
 url_prefix = 'https://api.telegram.org/bot' + tg_bot_token + '/'
 
 
 def getMe():
+    '''
+    :func getMe(): 用来验证token是否有效
+    '''
     url = url_prefix + 'getMe'
     r = requests.get(url)
     print(r.status_code)
@@ -17,24 +40,40 @@ def getMe():
 
 
 def sendMessage(chat_id, text):
+    '''
+    :func sendMessage(chat_id, text): https://core.telegram.org/bots/api#sendmessage 阅读此处
+    返回json
+    :param chat_id: telegram的id名，注意此id名是数字形式
+    '''
     url = url_prefix + 'sendMessage?chat_id={0}&text={1}'.format(chat_id, text)
     r = requests.get(url)
     return r.json()
 
 
 def getUpdates():
+    '''
+    :func getUpdates: https://core.telegram.org/bots/api#getupdates 阅读此处
+    返回json
+    '''
     url = url_prefix + 'getUpdates'
     r = requests.get(url)
     return r.json()
 
 
 def google_api(message):
+    '''
+    :func google_api: https://developers.google.com/web-search/docs/
+    返回json
+    '''
     url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + message
     r = requests.get(url)
     return r.json()
 
 
 def youdao_api(message):
+    '''
+    :func youdao_api: 和上面类似，调用api，返回json
+    '''
     url = 'http://fanyi.youdao.com/openapi.do?keyfrom={}&key={}&type=data&doctype=json&version=1.1&q={}'\
           .format(youdao_keyfrom, youdao_key, message.encode('utf-8'))
     r = requests.get(url)
@@ -42,6 +81,9 @@ def youdao_api(message):
 
 
 def nasa_api(message):
+    '''
+    同上
+    '''
     if message is None:
         url = 'https://api.nasa.gov/planetary/apod?concept_tags=True&api_key={}'\
               .format(nasa_api_key)
--- main.py
+++ main.py
@@ -3,24 +3,65 @@ from helper import getUpdates, google_api, sendMessage, youdao_api, nasa_api
 import collections
 import time
 
+'''
+:param update_id_deque: 这是一个长度为2的双队列，可在对列两端进行append，pop操作
+如果你不理解它的意思，看以下的例子
+
+>>> a = collections.deque(maxlen=2)
+>>> a
+deque([], maxlen=2)
+>>> a.append(1)
+>>> a
+deque([1], maxlen=2)
+>>> a.append(2)
+>>> a
+deque([1, 2], maxlen=2)
+>>> a.append(3)
+>>> a
+deque([2, 3], maxlen=2)
+
+'''
 update_id_deque = collections.deque(maxlen=2)
 chat_id = 0
 
 
 def main():
+    '''
+    用getUpdate方法，获取bot的消息列表
+    '''
     updates = getUpdates()
     rs = updates["result"]
     global update_id_deque
+    '''
+    update_id_deque每次都append，消息列表中最后一条信息的update_id，其中update_id是
+    自增长的，bot每收到一条消息，update_id都会加1，如果没有收到新消息，则update_id不变，
+    将update_id放入update_id_deque的目的是，通过计算update_id_deque[0]和update_id_deque[1]
+    的差，可以知道是否收到了新消息。
+    '''
     update_id_deque.append(rs[-1]['update_id'])
 
+    '''
+    update_id_deque长度为1时不做任何处理，这中情况出现在程序刚运行时
+    '''
     if len(update_id_deque) == 1:
-        print("len is 1")
         pass
+    '''
+    update_id_deque长度为2时，求出这个[1]与[0]之差
+    若差是0，这说明没有收到新的消息，
+    若差不是0，这说明收到了新的消息，由于我是间隔一定时间运行一次main函数，在这间隔期间
+    有可能收到不止一条消息。
+    :param delta: 则告诉你期间一共收到多少条消息
+    :param message_text: 接收到的消息
+    如果收到的消息以
+        /g开头，则执行google函数，返回json
+        /trans开头，则执行translate函数，返回json
+        /nasa开头，则执行nasa_pic函数，返回json
+        /help开头，则直接发送消息给用户
+    '''
     if len(update_id_deque) == 2:
         print("len is 2")
         delta = update_id_deque[1] - update_id_deque[0]
         if delta != 0:
-            print("delta is not 0")
             for _ in rs[-delta:]:
                 message_text = _['message']['text']
 
@@ -52,6 +93,9 @@ def main():
 
 
 def google(message):
+    '''
+    得到用户输入的内容，将google api 返回的结果发送给用户
+    '''
     google_rs_message = ''
     rs = google_api(message)['responseData']['results']
     for _ in rs[:1]:
@@ -60,6 +104,9 @@ def google(message):
 
 
 def translate(message):
+    '''
+    同上，不同的api，返回结果不同，如何处理需要查看它返回的json格式
+    '''
     rs = youdao_api(message)
     if rs['errorCode'] == 0:
         if 'basic' in rs:
@@ -80,6 +127,9 @@ def translate(message):
 
 
 def nasa_pic(message):
+    '''
+    同上
+    '''
     rs = nasa_api(message)
     if 'error' in rs:
         sendMessage(chat_id, rs['error'])
@@ -89,5 +139,9 @@ def nasa_pic(message):
 
 while True:
     main()
-    print("updating")
+    '''
+    每两秒执行一次，缺点显而易见，因为很多时候bot并没有接收到消息，但是
+    它还是执行了main函数，telegram提供了另外一种方式用来获取消息更新：webhook，
+    但是它要求必须是https，等我搞好https支持后再来优化
+    '''
     time.sleep(2)
