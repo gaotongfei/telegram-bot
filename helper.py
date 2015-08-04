90a9e90e9fda9200df821a368aff00f3b7fa27f8
gaotongfei <gaotongfei7@163.com>
2015-08-04 11:37:15 +0800

    add /help & /nasa command

--- helper.py
+++ helper.py
@@ -4,8 +4,10 @@ import os
 tg_bot_token = os.environ.get('tg_bot_token')
 youdao_keyfrom = os.environ.get('youdao_keyfrom')
 youdao_key = os.environ.get('youdao_key')
+nasa_api_key = os.environ.get('nasa_api_key')
 
 url_prefix = 'https://api.telegram.org/bot' + tg_bot_token + '/'
+
 
 def getMe():
     url = url_prefix + 'getMe'
@@ -13,22 +15,37 @@ def getMe():
     print(r.status_code)
     print(r.text)
 
+
 def sendMessage(chat_id, text):
     url = url_prefix + 'sendMessage?chat_id={0}&text={1}'.format(chat_id, text)
     r = requests.get(url)
     return r.json()
+
 
 def getUpdates():
     url = url_prefix + 'getUpdates'
     r = requests.get(url)
     return r.json()
 
+
 def google_api(message):
     url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + message
     r = requests.get(url)
     return r.json()
 
+
 def youdao_api(message):
-    url = 'http://fanyi.youdao.com/openapi.do?keyfrom={}&key={}&type=data&doctype=json&version=1.1&q={}'.format(youdao_keyfrom, youdao_key, message.encode('utf-8'))
+    url = 'http://fanyi.youdao.com/openapi.do?keyfrom={}&key={}&type=data&doctype=json&version=1.1&q={}'\
+          .format(youdao_keyfrom, youdao_key, message.encode('utf-8'))
+    r = requests.get(url)
+    return r.json()
+
+
+def nasa_api(message):
+    if message is None:
+        url = 'https://api.nasa.gov/planetary/apod?concept_tags=True&api_key={}'.format(nasa_api_key)
+    else:
+        url = 'https://api.nasa.gov/planetary/apod?concept_tags=True&api_key={}&date={}'\
+              .format(nasa_api_key, message)
     r = requests.get(url)
     return r.json()
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # coding=utf-8
-from helper import getUpdates, google_api, sendMessage, youdao_api
-import json
+from helper import getUpdates, google_api, sendMessage, youdao_api, nasa_api
 import collections
 import time
-import requests
 
 update_id_deque = collections.deque(maxlen=2)
 chat_id = 0
+
 
 def main():
     updates = getUpdates()
@@ -24,24 +23,41 @@ def main():
             print("delta is not 0")
             for _ in rs[-delta:]:
                 message_text = _['message']['text']
+
                 if message_text.startswith('/g'):
                     global chat_id
                     chat_id = _['message']['chat']['id']
                     google(message_text[3:])
                     update_id_deque.append(rs[-1]['update_id'])
-                    #print("1 ", update_id_deque[0],"2 ", update_id_deque[1])
+
                 elif message_text.startswith('/trans'):
                     chat_id = _['message']['chat']['id']
                     translate(message_text[7:])
                     update_id_deque.append(rs[-1]['update_id'])
+
+                elif message_text.startswith('/nasa'):
+                    chat_id = _['message']['chat']['id']
+                    if len(message_text.strip()) == 5:
+                        nasa_pic(message=None)
+                    else:
+                        nasa_pic(message_text[6:])
+                    update_id_deque.append(rs[-1]['update_id'])
+                elif message_text.startswith('/help'):
+                    chat_id = _['message']['chat']['id']
+                    help_message = str("命令列表：\n \
+                            * /g <关键词> google搜索 \n \
+                            * /trans <翻译的内容> \n \
+                            * /nasa [日期(若不填，默认为美国时间当日, 格式如2015-08-03)]")
+                    sendMessage(chat_id, help_message)
+
 
 def google(message):
     google_rs_message = ''
     rs = google_api(message)['responseData']['results']
     for _ in rs[:1]:
         google_rs_message += "{}\n{}".format(_['url'].encode('utf-8'), _['content'].encode('utf-8'))
-        #chat_id = '98496186'
         sendMessage(chat_id, str(google_rs_message))
+
 
 def translate(message):
     rs = youdao_api(message)
@@ -61,6 +77,15 @@ def translate(message):
         sendMessage(chat_id, u"无效的key")
     elif rs['errorCode'] == 60:
         sendMessage(chat_id, u"无词典结果，仅在获取词典结果生效")
+
+
+def nasa_pic(message):
+    rs = nasa_api(message)
+    if 'error' in rs:
+        sendMessage(chat_id, rs['error'])
+    else:
+        nasa_pic_and_explanation = "{}".format(rs['url'])
+        sendMessage(chat_id, nasa_pic_and_explanation)
 
 while True:
     main()
